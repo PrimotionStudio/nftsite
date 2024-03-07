d9d73905ee1076a5e9ca55a214437b1e921942b7
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-07 15:23:57 +0100

    sign-up sign-in verify explore

---
+++ account/includes/sidebar.php
@@ -0,0 +1,85 @@
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark" id="sidenav-main">
+	<div class="sidenav-header">
+		<i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+		<a class="navbar-brand m-0" href="dashboard">
+			<img src="assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo" />
+			<span class="ms-1 font-weight-bold text-white">NFT Site</span>
+		</a>
+	</div>
+	<hr class="horizontal light mt-0 mb-2" />
+	<div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link text-white active bg-gradient-primary" href="explore">
+					<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+						<i class="material-icons opacity-10">public</i>
+					</div>
+					<span class="nav-link-text ms-1">Explore</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link text-white" href="my-nfts">
+					<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+						<i class="material-icons opacity-10">collections</i>
+					</div>
+					<span class="nav-link-text ms-1">My NFTs</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link text-white" href="create-nfts">
+					<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+						<i class="material-icons opacity-10">add_to_photos</i>
+					</div>
+					<span class="nav-link-text ms-1">Create NFTs</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link text-white" href="bids">
+					<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+						<i class="material-icons opacity-10">notifications_none</i>
+					</div>
+					<span class="nav-link-text ms-1">Bids (0)</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link text-white" href="profile">
+					<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+						<i class="material-icons opacity-10">account_circle</i>
+					</div>
+					<span class="nav-link-text ms-1">Profile</span>
+				</a>
+			</li>
+			<li class="nav-item mt-3">
+				<h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">
+					Funds
+				</h6>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link text-white" href="add-funds">
+					<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+						<i class="material-icons opacity-10">attach_money</i>
+					</div>
+					<span class="nav-link-text ms-1">Add Funds</span>
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link text-white" href="withdraw">
+					<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+						<i class="material-icons opacity-10">arrow_upward</i>
+					</div>
+					<span class="nav-link-text ms-1">Withdraw</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div class="sidenav-footer position-absolute w-100 bottom-0">
+		<div class="mx-3">
+			<a class="btn bg-gradient-primary w-100" href="logout" type="button">
+				<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+					<i class="material-icons opacity-10">power_settings_new</i>
+				</div>
+				Sign Out
+			</a>
+		</div>
+	</div>
+</aside>
